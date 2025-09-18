e508e5bfc2acf3a20734a2a4da45deaa410a457b
Maksim Pankov <Pankov.M@digdes.com>
2025-09-18 15:17:44 +0300

    ShortFlatNamingStrategy: Resolve collisions

--- src/Naming/ShortFlatNamingStrategy.php
+++ src/Naming/ShortFlatNamingStrategy.php
@@ -7,6 +7,8 @@ use GoetasWebservices\XML\XSDReader\Schema\Type\Type;
 
 class ShortFlatNamingStrategy  extends ShortNamingStrategy
 {
+    private array $knownAnonymousTypeNames = [];
+
     public function getAnonymousTypeNamespace(PHPClass $class, PHPClass $parentClass): string {
         return $parentClass->getNamespace();
     }
@@ -14,5 +16,20 @@ class ShortFlatNamingStrategy  extends ShortNamingStrategy
     public function getAnonymousTypeNameForYaml(Type $type, string $typeNamespace, $parentClass, $parentName): string {
         $name = $this->getAnonymousTypeName($type, $parentName);
         return $typeNamespace . '\\' . $name;
+    }
+
+    public function getAnonymousTypeName(Type $type, $parentName)
+    {
+        $res = $this->classify($parentName) . 'AType';
+        if (array_key_exists($res, $this->knownAnonymousTypeNames)) {
+            $this->knownAnonymousTypeNames[$res] += 1;
+            $counter = $this->knownAnonymousTypeNames[$res];
+            $newName = $parentName . "_" . str($counter);
+            $res = $this->classify($newName) . 'AType';
+            return $res;
+        }
+
+        $this->knownAnonymousTypeNames[$res] = 1;
+        return $res;
     }
 }
