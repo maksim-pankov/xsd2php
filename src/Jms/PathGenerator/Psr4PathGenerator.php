5c695a2d819391cebdfaf739b3407ba119f00459
Maksim Pan'kov <Pankov.M@digdes.com>
2025-09-19 18:28:00 +0300

    Fix long meta filenames

--- src/Jms/PathGenerator/Psr4PathGenerator.php
+++ src/Jms/PathGenerator/Psr4PathGenerator.php
@@ -7,6 +7,8 @@ use GoetasWebservices\Xsd\XsdToPhp\PathGenerator\Psr4PathGenerator as Psr4PathGe
 
 class Psr4PathGenerator extends Psr4PathGeneratorBase implements PathGenerator
 {
+    private array $usedFileNames = [];
+
     public function getPath($yaml)
     {
         $ns = key($yaml);
@@ -18,9 +20,24 @@ class Psr4PathGenerator extends Psr4PathGeneratorBase implements PathGenerator
                 if (!is_dir($dir) && !$this->makeDirectory($dir)) {
                     throw new PathGeneratorException("Can't create the folder '$dir'");
                 }
-                $f = trim(strtr(substr($ns, strlen($namespace)), '\\/', '..'), '.');
 
-                return $dir . '/' . $f . '.yml';
+                $typeName = end(explode('\\', str_replace('/', '\\' , $ns)));
+                $fileName = $typeName;
+
+                $res = $dir . '/' . $fileName . '.yml';
+
+                $usedFileNames = $this->usedFileNames[$namespace] ?? [];
+
+                if (array_key_exists($res, $usedFileNames)) {
+                    $usedFileNames[$res] += 1;
+                    $fileName = $typeName . "_" . $usedFileNames[$res];
+                    $res = $dir . '/' . $fileName . '.yml';
+                } else {
+                    $usedFileNames[$res] = 1;
+                }
+
+                $this->usedFileNames[$namespace] = $usedFileNames;
+                return $res;
             }
         }
         throw new PathGeneratorException("Unable to determine location to save JMS metadata for class '$ns'");
--- src/Naming/ShortFlatNamingStrategy.php
+++ src/Naming/ShortFlatNamingStrategy.php
@@ -7,23 +7,14 @@ use GoetasWebservices\XML\XSDReader\Schema\Type\Type;
 
 class ShortFlatNamingStrategy  extends ShortNamingStrategy
 {
-    private array $knownAnonymousTypeNames = [];
+    
 
     public function getAnonymousTypeNamespace(PHPClass $class, PHPClass $parentClass): string {
         return $parentClass->getNamespace() . '\\' . $parentClass->getName();
     }
 
     public function getAnonymousTypeNameForYaml(Type $type, string $typeNamespace, $parentClass, $parentName): string {
-        $res = $typeNamespace . '\\' . $this->getAnonymousTypeName($type, $parentName);
-
-        if (array_key_exists($res, $this->knownAnonymousTypeNames)) {
-            $this->knownAnonymousTypeNames[$res] += 1;
-            $newName = $parentName . "_" . $this->knownAnonymousTypeNames[$res];
-            $res = $typeNamespace . '\\' . $this->getAnonymousTypeName($type, $newName);
-            return $res;
-        }
-
-        $this->knownAnonymousTypeNames[$res] = 1;
+        $res = $parentClass . '\\' . $this->getAnonymousTypeName($type, $parentName);
         return $res;
     }
 
