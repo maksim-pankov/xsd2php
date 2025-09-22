86f8355a026c11564ff4d166e6aa099bb88f1c32
Maksim Pan'kov <Pankov.M@digdes.com>
2025-09-22 19:58:35 +0300

    ShortFlat path generation fix

--- src/Jms/PathGenerator/ShortFlatPathGenerator.php
+++ src/Jms/PathGenerator/ShortFlatPathGenerator.php
@@ -5,8 +5,9 @@ namespace GoetasWebservices\Xsd\XsdToPhp\Jms\PathGenerator;
 use GoetasWebservices\Xsd\XsdToPhp\PathGenerator\PathGeneratorException;
 use GoetasWebservices\Xsd\XsdToPhp\PathGenerator\Psr4PathGenerator as Psr4PathGeneratorBase;
 
-class Psr4PathGenerator extends Psr4PathGeneratorBase implements PathGenerator
+class ShortFlatPathGenerator extends Psr4PathGeneratorBase implements PathGenerator
 {
+
     public function getPath($yaml)
     {
         $ns = key($yaml);
@@ -18,9 +19,16 @@ class Psr4PathGenerator extends Psr4PathGeneratorBase implements PathGenerator
                 if (!is_dir($dir) && !$this->makeDirectory($dir)) {
                     throw new PathGeneratorException("Can't create the folder '$dir'");
                 }
-                $f = trim(strtr(substr($ns, strlen($namespace)), '\\/', '..'), '.');
 
-                return $dir . '/' . $f . '.yml';
+                $fileName = ltrim( substr($ns, strlen($namespace)), '\\');
+                $fielPath = $dir . '/' . $fileName . '.yml';
+                $pathInfo = pathinfo($fielPath);
+                $dir = $pathInfo['dirname'];
+                if (!is_dir($dir) && !$this->makeDirectory($dir)) {
+                    throw new PathGeneratorException("Can't create the folder '$dir'");
+                }
+
+                return $fielPath;
             }
         }
         throw new PathGeneratorException("Unable to determine location to save JMS metadata for class '$ns'");
